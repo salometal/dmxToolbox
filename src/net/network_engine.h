1064234973b6e543088243c62211839954d60471
salometal <davidesalaffi@gmail.com>
2026-03-28 16:17:26 +0100

    refactor: network_engine spostato in src/net

---
+++ src/net/network_engine.h
@@ -0,0 +1,14 @@
+#ifndef NETWORK_ENGINE_H
+#define NETWORK_ENGINE_H
+
+#include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <WiFiUdp.h>
+#include <LittleFS.h>
+#include "../config.h"
+
+void saveConfiguration();
+void setupWebServer();
+void networkTask(void *pvParameters);
+
+#endif
